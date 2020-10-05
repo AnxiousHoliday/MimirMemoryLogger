bbb74ab5594319a7a1400c2d7d6a1c2887f2f9e6
Amer Eid <amereid92@gmail.com>
2020-10-05 23:33:34 +0300

    Improved example by adding a new button to get snapshots and add feedback to both buttons

--- Example/MimirMemoryLogger/ViewController.swift
+++ Example/MimirMemoryLogger/ViewController.swift
@@ -10,23 +10,28 @@ import UIKit
 import MimirMemoryLogger
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func takeMemorySnapshotTapped(_ sender: Any) {
         MimirMemoryLogger.saveCurrentSnapshotToFile { (url) -> (Void) in
             guard let url = url else {
                 return
             }
+            let alert = UIAlertController(title: "Success", message: "Memory snapshot taken successfully, location: \(url.absoluteString)\nLook at console for more details", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
             print("Latest memory snapshot location: \(url.absoluteString)")
+        }
+    }
+    
+    @IBAction func getAllMemorySnapshotsTapped(_ sender: Any) {
+        if let urls = MimirMemoryLogger.getSavedSnapshots() {
+            print("URLS of all saved snapshots: \(urls)")
+            let alert = UIAlertController(title: "Success", message: "Memory snapshots fetched, locations: \(urls)\nLook at console for more details", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "No saved snapshots found - Press the button above to take a memory snapshot first", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
 }
